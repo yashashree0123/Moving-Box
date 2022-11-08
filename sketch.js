8ea0a7c453bf8b0150ef39f78f6b7197bb1c13c0
yashashree0123 <117700343+yashashree0123@users.noreply.github.com>
2022-11-07 18:37:01 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box 
+function setup() {
+  createCanvas(800,800);
+box=createSprite(200,300,30,30)
+}
+
+function draw() 
+{
+  background(0);
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x=box.position.x+5
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x=box.position.x-5
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.position.y=box.position.y-5
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.position.y=box.position.y+5
+  }
+drawSprites()
+}
+
+
+
+
